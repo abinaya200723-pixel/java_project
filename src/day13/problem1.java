d7c64dad8b43d358f0bdcdcee8261477531766e5
abinaya200723-pixel <abinaya.2007.23@gmail.com>
2026-07-21 15:08:30 +0530

    new

--- src/day13/problem1.java
+++ src/day13/problem1.java
@@ -1,7 +1,37 @@
 package day13;
 
+class Counter {
+    private int count = 0;
+
+    public synchronized void increment() {
+        count++;
+        System.out.println(Thread.currentThread().getName() + " Count: " + count);
+    }
+}
+
+class MyThread extends Thread {
+    Counter counter;
+
+    MyThread(Counter counter, String name) {
+        this.counter = counter;
+        setName(name);
+    }
+
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            counter.increment();
+        }
+    }
+}
+
 public class problem1 {
-    static void main() {
-        System.out.println("hello");
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+
+        MyThread t1 = new MyThread(counter, "Thread-1");
+        MyThread t2 = new MyThread(counter, "Thread-2");
+
+        t1.start();
+        t2.start();
     }
 }
