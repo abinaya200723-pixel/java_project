1f2ac6270a00861f7f31c4bb091b57a409702efd
abinaya200723-pixel <abinaya.2007.23@gmail.com>
2026-07-22 14:18:34 +0530

    new

---
+++ src/day16/problem2.java
@@ -0,0 +1,17 @@
+package day16;
+
+public class problem2 {
+
+    public static void main(String[] args) {
+
+        int n = 3;
+
+        for(int i = 0; i < n; i++) {
+
+            for(int j = 0; j < n; j++) {
+                System.out.println(i + " " + j);
+            }
+
+        }
+    }
+}
---
+++ src/day16/problem3.java
@@ -0,0 +1,13 @@
+package day16;
+
+public class problem3{
+    public static void main(String[] args) {
+
+        int n = 16;
+
+        for(int i = 1; i < n; i = i * 2) {
+            System.out.println(i);
+        }
+
+    }
+}
